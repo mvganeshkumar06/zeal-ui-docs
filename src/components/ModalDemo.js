bec7e0ad678121706819b59ed2db4b95a95e6386
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-08-18 18:18:41 +0530

    updated component

--- src/components/ModalDemo.js
+++ src/components/ModalDemo.js
@@ -21,7 +21,7 @@ const ModalDemo = () => {
                     <Text type="subHeading">Modal</Text>
                     <Button onClick={onClose}>X</Button>
                 </Container>
-                <Text type="text">
+                <Text>
                     It is a long established fact that a reader will be
                     distracted by the readable content of a page when looking at
                     its layout. The point of using Lorem Ipsum is that it has a
@@ -40,7 +40,7 @@ const ModalDemo = () => {
                     <Text type="subHeading">Modal</Text>
                     <Button onClick={onClose}>X</Button>
                 </Container>
-                <Text type="text">
+                <Text>
                     It is a long established fact that a reader will be
                     distracted by the readable content of a page when looking at
                     its layout. The point of using Lorem Ipsum is that it has a
--- src/components/ToastDemo.js
+++ src/components/ToastDemo.js
@@ -2,69 +2,53 @@ import { Toast, useNotify, Text, Container, Button } from "@zeal-ui/core";
 
 const ToastDemo = () => {
     const { isOpen, onOpen, onClose } = useNotify();
-    const toast = `
-        width: 15rem;
-        @media(min-width:768px){
-            width:20rem;
-        }
-    `;
     return (
-        <Layout title="Toast - Zeal UI">
-            <Container type="col" width="100%">
-                <Text type="mainHeading">Toast</Text>
-                <Divider />
-                <Text type="subHeading">Import</Text>
-                <Text type="subHeading">Examples</Text>
-                <Container type="col">
-                    <Button onClick={() => onOpen("TOAST_LEFT")}>
-                        Show toast left
-                    </Button>
-                    <Button onClick={() => onOpen("TOAST_CENTER")}>
-                        Show toast center
-                    </Button>
-                    <Button onClick={() => onOpen("TOAST_RIGHT")}>
-                        Show toast right
-                    </Button>
-                </Container>
-                <Toast
-                    type="left"
-                    delay={3000}
-                    isOpen={isOpen === "TOAST_LEFT"}
-                    onClose={onClose}
-                    customStyles={toast}
-                >
-                    <Text type="text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt.
-                    </Text>
-                </Toast>
-                <Toast
-                    type="center"
-                    delay={3000}
-                    isOpen={isOpen === "TOAST_CENTER"}
-                    onClose={onClose}
-                    customStyles={toast}
-                >
-                    <Text type="text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt.
-                    </Text>
-                </Toast>
-                <Toast
-                    type="right"
-                    delay={5000}
-                    isOpen={isOpen === "TOAST_RIGHT"}
-                    onClose={onClose}
-                    customStyles={toast}
-                >
-                    <Text type="text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt.
-                    </Text>
-                </Toast>
-                <Text type="subHeading">Props</Text>
+        <Container type="col" width="100%">
+            <Container type="col">
+                <Button onClick={() => onOpen("TOAST_LEFT")}>
+                    Toast left
+                </Button>
+                <Button onClick={() => onOpen("TOAST_CENTER")}>
+                    Toast center
+                </Button>
+                <Button onClick={() => onOpen("TOAST_RIGHT")}>
+                    Toast right
+                </Button>
             </Container>
-        </Layout>
+            <Toast
+                type="left"
+                delay={3000}
+                isOpen={isOpen === "TOAST_LEFT"}
+                onClose={onClose}
+                color="blue"
+            >
+                <Text>
+                    Lorem ipsum dolor sit amet.
+                </Text>
+            </Toast>
+            <Toast
+                type="center"
+                delay={3000}
+                isOpen={isOpen === "TOAST_CENTER"}
+                onClose={onClose}
+                color="blue"
+            >
+                <Text>
+                    Lorem ipsum dolor sit amet.
+                </Text>
+            </Toast>
+            <Toast
+                type="right"
+                delay={3000}
+                isOpen={isOpen === "TOAST_RIGHT"}
+                onClose={onClose}
+                color="blue"
+            >
+                <Text>
+                    Lorem ipsum dolor sit amet.
+                </Text>
+            </Toast>
+        </Container>
     );
 };
 
