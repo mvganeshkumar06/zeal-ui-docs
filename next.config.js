6447e77a6a3f72cb0206ecadd7a75051fe0edbe8
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-01-27 20:40:40 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx$/
-});
-
-const config = withMDX({
-    pageExtensions: ['js', 'jsx', 'mdx']
-});
+const config = {
+	pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+	webpack(config, options) {
+		config.module.rules.push({
+			test: /\.mdx?$/,
+			use: [
+				options.defaultLoaders.babel,
+				{
+					loader: '@mdx-js/loader',
+				},
+			],
+		});
+		return config;
+	},
+};
 
 module.exports = config;
